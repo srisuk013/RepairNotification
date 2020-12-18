7f7808a726cafd79ac83dfd1974ab1736e638cf3
AdeDom <pathiphonyw@gmail.com>
2020-12-18 13:31:48 +0700

    chicken not

--- app/src/main/java/com/srisuk/repairnotification/MainActivity.kt
+++ app/src/main/java/com/srisuk/repairnotification/MainActivity.kt
@@ -2,10 +2,17 @@ package com.srisuk.repairnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "${chickenNot()}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun chickenNot(): String {
+        return "Hello chicken not"
     }
 }
